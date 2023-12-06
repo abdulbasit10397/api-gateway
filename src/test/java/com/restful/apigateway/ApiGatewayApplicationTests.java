25ef9042c5e466e2d59b3417b3896af1a0ae7574
Abdul Basit <abdulbasit10397@gmail.com>
2023-12-06 21:54:45 +0500

    APG-03 Added the test class

---
+++ src/test/java/com/restful/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restful.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
